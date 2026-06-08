6870f27368fda9ee18a87d0fac7e4851d3f32208
youssefkhalil10 <youssefkhalil510@gmail.com>
2026-06-08 22:56:01 +0300

    Adding Booking Page

---
+++ src/features/bookings/useBooking.js
@@ -0,0 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import { getBooking } from "../../services/apiBookings";
+import { useParams } from "react-router-dom";
+
+export function useBooking() {
+  const { bookingId } = useParams();
+  const {
+    isLoading,
+    data: booking,
+    error,
+  } = useQuery({
+    queryKey: ["booking"],
+    queryFn: () => getBooking(bookingId),
+    //some times react query try to refetch the data that does not exists about 3 times.
+    retry: false,
+  });
+
+  return { isLoading, booking, error };
+}
