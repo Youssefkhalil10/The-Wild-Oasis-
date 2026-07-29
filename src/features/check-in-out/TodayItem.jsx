843272202a7ed47d8972551c34e4d69135649f35
youssefkhalil10 <youssefkhalil510@gmail.com>
2026-07-29 22:37:57 +0300

    Feat: add TodayActivity to Dashboard

--- src/features/check-in-out/TodayItem.jsx
+++ src/features/check-in-out/TodayItem.jsx
@@ -1,5 +1,10 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
+import CheckoutButton from "./CheckoutButton";
+import Button from "../../ui/Button";
+import Tag from "../../ui/Tag";
+import Flag from "../../ui/Flag";
 const StyledTodayItem = styled.li`
   display: grid;
   grid-template-columns: 9rem 2rem 1fr 7rem 9rem;
@@ -18,3 +23,30 @@ const StyledTodayItem = styled.li`
 const Guest = styled.div`
   font-weight: 500;
 `;
+
+function TodayItem({ activity }) {
+  const { id, guests, status, numNights } = activity;
+  return (
+    <StyledTodayItem>
+      {status === "unconfirmed" && <Tag type={"green"}>Arriving</Tag>}
+      {status === "checked-in" && <Tag type={"blue"}>Departing</Tag>}
+      <Flag src={guests.countryFlag} alt={`Flag of ${guests.nationality}`} />
+      <Guest>{guests.fullName}</Guest>
+      <div>{numNights} nights</div>
+      {status === "unconfirmed" ? (
+        <Button
+          size="small"
+          variation="primary"
+          as={Link}
+          to={`/checkin/${id}`}
+        >
+          Check in
+        </Button>
+      ) : (
+        <CheckoutButton bookingId={id}>Check out</CheckoutButton>
+      )}
+    </StyledTodayItem>
+  );
+}
+
+export default TodayItem;
